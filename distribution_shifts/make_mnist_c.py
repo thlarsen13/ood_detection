0d635826b87f5b8f6f90a624378d76aaef6fccf0
thlarsen13 <thomas.larsen031@gmail.com>
2022-01-31 21:35:41 -0500

    making references less circular, allowing for stuff to be called outside the venv ofor image shifting

--- distribution_shifts/make_mnist_c.py
+++ distribution_shifts/make_mnist_c.py
@@ -496,7 +496,8 @@ folder_path = '/home/thlarsen/ood_detection/distribution_shifts/mnist_c/'
 
 def load_mnist_c(method_name): 
 	data = np.load(folder_path + d[method_name].__name__ + '.npy')
-	labels =     np.load(folder_path + 'labels.npy')
-	return data, labels
+	labels = np.load(folder_path + 'labels.npy')
+    sev = np.load(folder_path + d[method_name].__name__ + '_sev.npy')
+	return data, labels, sev
 
 
